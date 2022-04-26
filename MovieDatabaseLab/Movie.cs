16da866dbc824be626e1f80a37d0d3560041f932
prestonf2003 <prestonfernando@gmail.com>
2022-04-25 23:54:41 -0400

    Add project files.

---
+++ MovieDatabaseLab/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabaseLab
+{
+     class Movie
+    {
+        public string title { get; set; }
+        public string category { get; set; }
+        public Movie(string title, string category)
+        {
+            this.title = title;
+            this.category = category;
+        }
+
+    }
+}
---
+++ MovieDatabaseLab/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+
+namespace MovieDatabaseLab
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            bool DidNotFuckUp = true;
+            Movie thisMovie;
+            while (true)
+            {
+                List<Movie> movieList = new List<Movie>();
+                movieList.Add(new Movie("Star Wars", "Fantasy"));
+                movieList.Add(new Movie("Enders Game", "Science Fiction"));
+                movieList.Add(new Movie("The Mummy", "Action"));
+                movieList.Add(new Movie("Daddy Day Care", "Comedy"));
+                movieList.Add(new Movie("water Boy", "Comedy"));
+                movieList.Add(new Movie("8 Mile", "Drama"));
+                movieList.Add(new Movie("Poccahontas", "Animation"));
+                movieList.Add(new Movie("A Cabin in the woods", "Horror"));
+                movieList.Add(new Movie("Free Willy", "Drama"));
+                movieList.Add(new Movie("Freaky Friday", "Comedy"));
+
+                string Category = GetUserInput("Please enter a category for a movie");
+
+
+
+
+                bool foundOne = false;
+                for (int i = 0; i < movieList.Count; i++)
+                {
+                     thisMovie = movieList[i];
+                    if (thisMovie.category == Category)
+                    {
+                        Console.WriteLine(thisMovie.title);
+                        foundOne = true;
+                    }
+                }
+                if (foundOne)
+                {
+                    break;
+                }
+
+
+            }
+
+        }
+        public static string GetUserInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string output = Console.ReadLine();
+            return output;
+        }
+    }
+}
